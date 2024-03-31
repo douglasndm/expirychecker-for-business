0f56293dc69a897d27b1703d18c88b7d6a1675ab
Douglas <7841509+douglasndm@users.noreply.github.com>
2024-03-31 08:36:17 -0300

    fix: excel export types

--- src/@types/Utils/Export/Excel.d.ts
+++ src/@types/Utils/Export/Excel.d.ts
@@ -1,21 +1,20 @@
 interface ExcelRowProps {
-    produto: string;
-    codigo: string;
-    marca: string;
-    lote: string;
-    vencimento: Date;
-    quantidade: number;
-    preço: number;
+	produto: string;
+	codigo: string;
+	marca: string;
+	lote: string;
+	vencimento: Date;
+	quantidade: number;
+	preço: number;
 }
 
 interface exportModel {
-    product: Omit<IProduct, 'Lotes'>;
-    batch: IBatch;
+	product: Omit<IProduct, 'Lotes'>;
+	batch: IBatch;
 }
 
 interface exportProps {
-    sortBy: 'created_date' | 'expire_date';
-    brand?: string | null;
-    category?: string | null;
-    store?: string | null;
+	brand?: string | null;
+	category?: string | null;
+	store?: string | null;
 }
