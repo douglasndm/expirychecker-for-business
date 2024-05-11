6a4281f3c844c8073e39026f09d2c55d78f74f00
Douglas <7841509+douglasndm@users.noreply.github.com>
2024-05-11 11:46:49 -0300

    Upgrading to RN 0.74.1 with yarn 3

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ import { AppRegistry } from 'react-native';
 
 import '@services/Bugsnag';
 import { requestNotificationPermission } from '@services/Notifications/Permission';
-import '@services/Notifications';
 import '@services/PushNotificationHandler';
 
 import { name as appName } from './app.json';
 import App from './src';
 import './src/Functions/OpenAppTimes';
+
+if (__DEV__) {
+	require('@services/Reactotron');
+}
 
 requestNotificationPermission();
 
