81b56a5377786cabc17c05ce8eec87a363ea0f84
Douglas <7841509+douglasndm@users.noreply.github.com>
2024-05-27 12:47:20 -0300

    Version bump 1.10.10

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 import { AppRegistry } from 'react-native';
 
 import '@services/Bugsnag';
-import { requestNotificationPermission } from '@services/Notifications/Permission';
-import '@services/PushNotificationHandler';
+// import '@services/PushNotificationHandler';
 
 import { name as appName } from './app.json';
 import App from './src';
+
 import './src/Functions/OpenAppTimes';
 
 if (__DEV__) {
 	require('@services/Reactotron');
 }
-
-requestNotificationPermission();
 
 AppRegistry.registerComponent(appName, () => App);
