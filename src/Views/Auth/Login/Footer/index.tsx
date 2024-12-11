14d79661eff0ccad4513f3fb88d830c62d86f2e1
Douglas <7841509+douglasndm@users.noreply.github.com>
2024-12-10 21:24:14 -0300

    Updating privacy link

--- src/Views/Auth/Login/Footer/index.tsx
+++ src/Views/Auth/Login/Footer/index.tsx
@@ -1,41 +1,44 @@
 import React, { useCallback } from 'react';
 import { Linking } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import { StackNavigationProp } from '@react-navigation/stack';
 
-import strings from '~/Locales';
+import strings from '@teams/Locales';
 
 import { AboutContainer, CreateAccountText, Text, Link } from './styles';
 
 const Footer: React.FC = () => {
-    const { navigate } = useNavigation();
+	const { navigate } = useNavigation<StackNavigationProp<RoutesParams>>();
 
-    const handleNavigateToCreateAcc = useCallback(() => {
-        navigate('CreateAccount');
-    }, [navigate]);
+	const handleNavigateToCreateAcc = useCallback(() => {
+		navigate('CreateAccount');
+	}, [navigate]);
 
-    const navigateToTerms = useCallback(async () => {
-        await Linking.openURL('https://douglasndm.dev/terms');
-    }, []);
+	const navigateToTerms = useCallback(async () => {
+		await Linking.openURL('https://douglasndm.dev/terms');
+	}, []);
 
-    const navigateToPrivacy = useCallback(async () => {
-        await Linking.openURL('https://douglasndm.dev/privacy');
-    }, []);
+	const navigateToPrivacy = useCallback(async () => {
+		await Linking.openURL(
+			'https://controledevalidades.com/politica-de-privacidade/'
+		);
+	}, []);
 
-    return (
-        <AboutContainer>
-            <CreateAccountText onPress={handleNavigateToCreateAcc}>
-                {strings.View_Login_Label_CreateAccount}
-            </CreateAccountText>
+	return (
+		<AboutContainer>
+			<CreateAccountText onPress={handleNavigateToCreateAcc}>
+				{strings.View_Login_Label_CreateAccount}
+			</CreateAccountText>
 
-            <Text>
-                {strings.BeforeTermsAndPrivacy}
-                <Link onPress={navigateToTerms}>{strings.Terms}</Link>
-                {strings.BetweenTermsAndPrivacy}
-                <Link onPress={navigateToPrivacy}>{strings.PrivacyPolicy}</Link>
-                .
-            </Text>
-        </AboutContainer>
-    );
+			<Text>
+				{strings.BeforeTermsAndPrivacy}
+				<Link onPress={navigateToTerms}>{strings.Terms}</Link>
+				{strings.BetweenTermsAndPrivacy}
+				<Link onPress={navigateToPrivacy}>{strings.PrivacyPolicy}</Link>
+				.
+			</Text>
+		</AboutContainer>
+	);
 };
 
 export default Footer;
