ce0ff802aafab4f1614e2f6af0076bce0ab96390
Douglas <7841509+douglasndm@users.noreply.github.com>
2025-01-15 03:19:08 -0300

    enabling more sentry features

--- index.js
+++ index.js
@@ -11,19 +11,23 @@ import './src/Functions/OpenAppTimes';
 
 const reactNavigationIntegration =  Sentry.reactNavigationIntegration();
 
-if (!__DEV__) {
-    Sentry.init({
-        dsn: EnvConfig.SENTRY_DSN,
-        // Set tracesSampleRate to 1.0 to capture 100% of transactions for tracing.
-        tracesSampleRate: 1.0,
-        enableAppStartTracking: true,
-        enableNativeFramesTracking: true,
-        enableStallTracking: true,
-        enableUserInteractionTracing: true,
-        integrations: [
-            reactNavigationIntegration
-        ],
-    });
-}
+Sentry.init({
+    dsn: EnvConfig.SENTRY_DSN,
+    // uncomment the line below to enable Spotlight (https://spotlightjs.com)
+    enableSpotlight: __DEV__,
+
+    // Set tracesSampleRate to 1.0 to capture 100% of transactions for tracing.
+    enabled: !__DEV__,
+    enableCaptureFailedRequests: !__DEV__,
+    tracesSampleRate: 1.0,
+    enableAppStartTracking: true,
+    enableNativeFramesTracking: true,
+    enableStallTracking: true,
+    enableUserInteractionTracing: true,
+    integrations: [
+        reactNavigationIntegration
+    ],
+});
+
 
 AppRegistry.registerComponent(appName, () => Sentry.wrap(App));
